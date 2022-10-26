e8916f65e8f5e36e930e3384fd963992f19cbcb9
juhee <77ooee@gmail.com>
2022-10-26 19:52:22 +0900

    feat: msw 세팅

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { RecoilRoot } from "recoil";
+import { worker } from "./mocks/browser";
 import App from "./App";
 import "./index.css";
 import "./assets/styles";
+
+if (process.env.NODE_ENV === "development") {
+  worker.start();
+}
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/mocks/handlers.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { rest } from "msw";
+
+export const handlers = [];
