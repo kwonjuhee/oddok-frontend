0130fe956c40e84a2323bfaa6b8d4c06e81c9d48
juhee <77ooee@gmail.com>
2022-11-06 17:28:53 +0900

    refactor: 라우터 기반 코드 스플리팅 적용

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,37 +1,42 @@
+import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-import CreateRoom from "@pages/CreateRoom";
-import JoinRoom from "@pages/JoinRoom";
-import Main from "@pages/Main/Main";
-import Login from "@pages/Login/Login";
-import Search from "@pages/Search/Search";
-import MyPage from "@pages/MyPage/MyPage";
-import StudyRoom from "@pages/StudyRoom/StudyRoom";
-import NotFoundPage from "@pages/NotFoundPage/NotFoundPage";
-import RedirectPage from "@pages/Login/RedirectPage";
-import LogoutRedirectPage from "@pages/LogoutRedirectPage/LogoutRedirectPage";
-import ShareStudyTime from "@pages/ShareStudyTime/ShareStudyTime";
+
 import PublicRoute from "./PublicRoute";
 import PrivateRoute from "./PrivateRoute";
 
+const Main = lazy(() => import("@pages/Main/Main"));
+const Login = lazy(() => import("@pages/Login/Login"));
+const RedirectPage = lazy(() => import("@pages/Login/RedirectPage"));
+const LogoutRedirectPage = lazy(() => import("@pages/LogoutRedirectPage/LogoutRedirectPage"));
+const Search = lazy(() => import("@pages/Search/Search"));
+const MyPage = lazy(() => import("@pages/MyPage/MyPage"));
+const CreateRoom = lazy(() => import("@pages/CreateRoom"));
+const JoinRoom = lazy(() => import("@pages/JoinRoom"));
+const StudyRoom = lazy(() => import("@pages/StudyRoom/StudyRoom"));
+const ShareStudyTime = lazy(() => import("@pages/ShareStudyTime/ShareStudyTime"));
+const NotFoundPage = lazy(() => import("@pages/NotFoundPage/NotFoundPage"));
+
 function Router() {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/search/*" element={<Search />} />
-      <Route element={<PublicRoute />}>
-        <Route path="/login" element={<Login />} />
-        <Route path="/login/oauth2/code/kakao" element={<RedirectPage />} />
-        <Route path="/logout/oauth2/code/kakao" element={<LogoutRedirectPage />} />
-      </Route>
-      <Route element={<PrivateRoute />}>
-        <Route path="/studyroom/create" element={<CreateRoom />} />
-        <Route path="/studyroom/:roomId/setting" element={<JoinRoom />} />
-        <Route path="/studyroom/:roomId" element={<StudyRoom />} />
-        <Route path="/share/study-time" element={<ShareStudyTime />} />
-        <Route path="/mypage" element={<MyPage />} />
-      </Route>
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <Suspense fallback={null}>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/search/*" element={<Search />} />
+        <Route element={<PublicRoute />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/login/oauth2/code/kakao" element={<RedirectPage />} />
+          <Route path="/logout/oauth2/code/kakao" element={<LogoutRedirectPage />} />
+        </Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/studyroom/create" element={<CreateRoom />} />
+          <Route path="/studyroom/:roomId/setting" element={<JoinRoom />} />
+          <Route path="/studyroom/:roomId" element={<StudyRoom />} />
+          <Route path="/share/study-time" element={<ShareStudyTime />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   );
 }
 
